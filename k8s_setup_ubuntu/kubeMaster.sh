cc584ab23f68c98ec00ba66b2556feaacb688006
suren2306 <msurendar.suren@gmail.com>
2018-10-01 15:44:02 +0530

    first commit

---
+++ k8s_setup_ubuntu/kubeMaster.sh
@@ -0,0 +1,9 @@
+swapoff -a
+kubeadm config images pull
+kubeadm init --pod-network-cidr=192.168.0.0/16
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+
---
+++ k8s_setup_ubuntu/token.sh
@@ -0,0 +1,1 @@
+kubeadm join <The joining tocken/ The one you got after setting up the master node>
